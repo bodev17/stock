b4f50302cc05b04870d291fe168c3e601faeda7d
truongnq72 <truongnq72@ghtk.co>
2024-06-17 15:40:05 +0700

    update

--- app/Repository/SymbolRepository.ts
+++ app/Repository/SymbolRepository.ts
@@ -2,7 +2,9 @@ import SymbolRepositoryInterface from "App/Interface/SymbolRepositoryInterface";
 import Symbol from "App/Models/Symbol";
 import {SymbolStatus} from "App/Enum/SymbolStatus";
 import BaseRepository from "App/Repository/BaseRepository";
+import {inject} from "@adonisjs/fold";
 
+@inject()
 export default class SymbolRepository implements SymbolRepositoryInterface {
   public async getActiveSymbols(): Promise<any> {
     const symbol = await Symbol.all()
